88c5f8dd251390ef462bb9ae1118c12218da5709
Anton Glyzin <tosha.glyzin@mail.ru>
2025-09-02 10:25:48 +0300

    fix: запуск deployments pipy

--- tests/test_files_box.py
+++ tests/test_files_box.py
@@ -20,7 +20,7 @@ class TestFilesBox(BaseTest):
         cls.fb.clear()
     
     def test_case_1(self):
-        logger.info('1. Запись и получение данных из одного потока.')
+        logger.info('1. Запись и получение данных из одного потока. ')
         logger.info('----Запись значений в очередь.')
         data1 = {'id': 1, 'name': 'Leanne Graham', 'username': 'Bret', 'email': 'Sincere@april.biz', 'address': {'street': 'Kulas Light', 'suite': 'Apt. 556', 'city': 'Gwenborough', 'zipcode': '92998-3874', 'geo': {'lat': '-37.3159', 'lng': '81.1496'}}, 'phone': '1-770-736-8031 x56442', 'website': 'hildegard.org', 'company': {'name': 'Romaguera-Crona', 'catchPhrase': 'Multi-layered client-server neural-net', 'bs': 'harness real-time e-markets'}}
         key1 = self.fb.put(json.dumps(data1))
